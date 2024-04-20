3824965f03810ab0187ad1898ca12f5197ab926d
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2024-04-20 17:05:08 +0800

    style: Update code formatting and remove unnecessary semicolons

--- packages/client/src/components/VoiceChannelActivity.tsx
+++ packages/client/src/components/VoiceChannelActivity.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import {Player} from './Player';
-import {usePlayers} from '../hooks/usePlayers';
-import './VoiceChannelActivity.css';
+import * as React from 'react'
+import { Player } from './Player'
+import { usePlayers } from '../hooks/usePlayers'
+import './VoiceChannelActivity.css'
 
 export function VoiceChannelActivity() {
-  const players = usePlayers();
+    const players = usePlayers()
 
-  return (
-    <div className="voice__channel__container">
-      {players.map((p) => (
-        <Player key={p.userId} {...p} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="voice__channel__container">
+            {players.map((p) => (
+                <Player key={p.userId} {...p} />
+            ))}
+        </div>
+    )
 }
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,5 +1,5 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // NOTE: building for prod will not work with a linked package
 // You should install directly from the repo in order to make the build work
@@ -8,20 +8,20 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  envDir: '../../',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+    plugins: [react()],
+    envDir: '../../',
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3001',
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+        hmr: {
+            clientPort: 443,
+        },
     },
-    hmr: {
-      clientPort: 443,
-    },
-  },
-});
+})
