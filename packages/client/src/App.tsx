74e779e298905317b941a88948dcbb689d1548e1
Sheila Mae Craus <68653710+sheicraus@users.noreply.github.com>
2024-04-22 22:54:42 +0800

    chore: install packages

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,11 +3,24 @@ import { AuthenticatedContextProvider } from './hooks/useAuthenticatedContext'
 import { PlayersContextProvider } from './hooks/usePlayers'
 
 import { VoiceChannelActivity } from './components/VoiceChannelActivity'
+import GridBackground from './components/GridBackground'
+import { Col, Row } from 'antd'
 export default function App() {
     return (
         <AuthenticatedContextProvider>
             <PlayersContextProvider>
-                <VoiceChannelActivity />
+                {/* <GridBackground /> */}
+                <Row>
+                    <Col span={8}>
+                        <Row></Row>
+                        <Row></Row>
+                        <Row></Row>
+                    </Col>
+                    <Col span={16}>
+                        <Row></Row>
+                    </Col>
+                </Row>
+                {/* <VoiceChannelActivity /> */}
             </PlayersContextProvider>
         </AuthenticatedContextProvider>
     )
