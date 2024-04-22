58717c57a36fca6c7413a1902cfb45a592426881
Sheila Mae Craus <68653710+sheicraus@users.noreply.github.com>
2024-04-22 23:25:02 +0800

    Revert "chore: install packages"
    This reverts commit 74e779e298905317b941a88948dcbb689d1548e1.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,24 +3,11 @@ import { AuthenticatedContextProvider } from './hooks/useAuthenticatedContext'
 import { PlayersContextProvider } from './hooks/usePlayers'
 
 import { VoiceChannelActivity } from './components/VoiceChannelActivity'
-import GridBackground from './components/GridBackground'
-import { Col, Row } from 'antd'
 export default function App() {
     return (
         <AuthenticatedContextProvider>
             <PlayersContextProvider>
-                {/* <GridBackground /> */}
-                <Row>
-                    <Col span={8}>
-                        <Row></Row>
-                        <Row></Row>
-                        <Row></Row>
-                    </Col>
-                    <Col span={16}>
-                        <Row></Row>
-                    </Col>
-                </Row>
-                {/* <VoiceChannelActivity /> */}
+                <VoiceChannelActivity />
             </PlayersContextProvider>
         </AuthenticatedContextProvider>
     )
--- packages/client/src/components/VoiceChannelActivity.tsx
+++ packages/client/src/components/VoiceChannelActivity.tsx
@@ -2,17 +2,15 @@ import * as React from 'react'
 import { Player } from './Player'
 import { usePlayers } from '../hooks/usePlayers'
 import './VoiceChannelActivity.css'
-import Desk from './Desk'
-import Nook from './Nook'
 
 export function VoiceChannelActivity() {
     const players = usePlayers()
 
     return (
         <div className="voice__channel__container">
-            {/* {players.map((p) => (
-                <Nook key={p.userId} {...p} />
-            ))} */}
+            {players.map((p) => (
+                <Player key={p.userId} {...p} />
+            ))}
         </div>
     )
 }
