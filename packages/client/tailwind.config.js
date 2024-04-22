60fd304568aec398edb9420d7de41378986a608c
Sheila Mae Craus <68653710+sheicraus@users.noreply.github.com>
2024-04-22 23:24:48 +0800

    Revert "chore: tailwind config"
    This reverts commit b66462a929dde84785ec0407e4153c9dcd69184c.

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [],
   theme: {
     fontFamily: {
       sans: ['Quicksand', 'sans-serif'],
@@ -9,3 +9,4 @@ module.exports = {
   },
   plugins: [],
 }
+
