343e07068991519bb4d5ee5a13a4fa948cf7e7d6
Sheila Mae Craus <68653710+sheicraus@users.noreply.github.com>
2024-04-23 21:18:17 +0800

    feat: layout

--- packages/client/src/components/MainActivity.tsx
+++ packages/client/src/components/MainActivity.tsx
@@ -1,18 +1,16 @@
-import { Col, Row } from 'antd'
+import { Card, Col, Flex, Row } from 'antd'
 export function MainActivity() {
     return (
-        <Row className="w-full h-full">
-            <Col span={6} className="bg-slate-200">
-                <Row gutter={[8, 8]}>
-                    <Col span={24} className='bg-red-100 flex'>user row</Col>
-                    <Col span={24} className='bg-red-200'>timer row</Col>
-                    <Col span={24} className='bg-red-300'>todo row</Col>
-                    <Col span={24} className='bg-red-400'>music row</Col>
-                </Row>
-            </Col>
-            <Col span={18} className="bg-slate-500 h-full">
-                main room
-            </Col>
-        </Row>
+        <Flex className='w-full'>
+            <Flex vertical className="p-2 h-screen w-1/3">
+                <Card className="m-1 h-1/6">user row</Card>
+                <Card className="m-1 h-2/4">timer row</Card>
+                <Card className="m-1 h-2/4">todo row</Card>
+                <Card className="m-1 h-1/6">music row</Card>
+            </Flex>
+            <Flex vertical className="p-2 h-screen w-2/3">
+                <Card className="m-1 h-full">main activity space</Card>
+            </Flex>
+        </Flex>
     )
 }
