a68dccb81de5da1dee68b059a2a8357f5e97825b
Sheila Mae Craus <68653710+sheicraus@users.noreply.github.com>
2024-04-23 21:21:14 +0800

    chore: styles

--- packages/client/src/components/MainActivity.tsx
+++ packages/client/src/components/MainActivity.tsx
@@ -1,11 +1,11 @@
 import { Card, Col, Flex, Row } from 'antd'
 export function MainActivity() {
     return (
-        <Flex className='w-full'>
+        <Flex className='w-full' gap={1}>
             <Flex vertical className="p-2 h-screen w-1/3">
                 <Card className="m-1 h-1/6">user row</Card>
-                <Card className="m-1 h-2/4">timer row</Card>
-                <Card className="m-1 h-2/4">todo row</Card>
+                <Card className="m-1 h-2/6">timer row</Card>
+                <Card className="m-1 h-2/6">todo row</Card>
                 <Card className="m-1 h-1/6">music row</Card>
             </Flex>
             <Flex vertical className="p-2 h-screen w-2/3">
