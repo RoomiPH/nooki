c75d0b71e3858201b88b6ba91522ed8edfe0dc43
crsplrb <crispauljohn.rubi@gmail.com>
2024-04-30 22:13:46 +0800

    chore: semicolon in Player.ts

--- packages/server/src/entities/Player.ts
+++ packages/server/src/entities/Player.ts
@@ -1,32 +1,32 @@
-import { Schema, type } from '@colyseus/schema'
+import { Schema, type } from '@colyseus/schema';
 
 export type TPlayerOptions = Pick<
     Player,
     'sessionId' | 'userId' | 'name' | 'avatarUri' | 'talking'
->
+>;
 
 export class Player extends Schema {
     @type('string')
-    public sessionId: string
+    public sessionId: string;
 
     @type('string')
-    public userId: string
+    public userId: string;
 
     @type('string')
-    public avatarUri: string
+    public avatarUri: string;
 
     @type('string')
-    public name: string
+    public name: string;
 
     @type('boolean')
-    public talking: boolean = false
+    public talking: boolean = false;
 
     // Init
     constructor({ name, userId, avatarUri, sessionId }: TPlayerOptions) {
-        super()
-        this.userId = userId
-        this.avatarUri = avatarUri
-        this.name = name
-        this.sessionId = sessionId
+        super();
+        this.userId = userId;
+        this.avatarUri = avatarUri;
+        this.name = name;
+        this.sessionId = sessionId;
     }
 }
